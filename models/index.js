260e30bc60dde29e5d104b4c86870ca9e777e4b5
bhwong11 <user@email.com>
2021-12-13 18:08:40 -0500

    added atlas connection

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//const connectionString = process.env.MONGO_URI || "mongodb://localhost:27017/delivery-app";
+const connectionString = process.env.MONGO_URI;
+const configOptions = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}
+
+mongoose.connect(connectionString, configOptions)
+.then(
+    ()=>console.log("MongoDB successfully connected...")
+)
+.catch(
+    ()=>{
+        (err)=> console.log(`MongoDB Connection Error: ${err}`)
+    }
+)
+
+//module.exports here
+
